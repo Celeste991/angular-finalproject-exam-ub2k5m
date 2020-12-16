d036149961aeb02ca9d2f97da7798ff4488b9e5f
Celeste991 <celeste.briceno92@student.qcc.cuny.edu>
2020-12-16 00:02:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-finalproject-exam-ub2k5m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-finalproject-exam-ub2k5m)
---
+++ src/app/products.ts
@@ -0,0 +1,54 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+/* images */
+export const blog = {
+
+  /* images */
+  "image": {
+
+  "me": "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/131441449_1109694462842504_8964627852119966744_o.jpg?_nc_cat=100&ccb=2&_nc_sid=730e14&_nc_ohc=aA6Kfwc6wB8AX_yvb8k&_nc_ht=scontent-lga3-1.xx&oh=a1bcd9b06c4164362885b63868a72a83&oe=5FFD9401",
+
+  "me2": "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/131896950_1109695176175766_7342537654598947097_o.jpg?_nc_cat=108&ccb=2&_nc_sid=730e14&_nc_ohc=hl1_o76TzN4AX-7uPZr&_nc_ht=scontent-lga3-1.xx&oh=aa8009a1023d27ef5e56293633aed439&oe=5FFFDD8D",
+
+  "me3": "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/131893023_1109695192842431_4651280400315789484_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=eJfO4q4qiUcAX_kK483&_nc_ht=scontent-lga3-1.xx&oh=bcf4b758c8bcab5e5a01d74138c9c4c6&oe=60001ACA",
+
+  "me4": "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/131366894_1109696759508941_399613825375964895_o.jpg?_nc_cat=102&ccb=2&_nc_sid=730e14&_nc_ohc=UKvc4BRkPQUAX-Orw4h&_nc_ht=scontent-lga3-1.xx&oh=e7c0d141ee77f2a09339dc3bb1e3a352&oe=60002102",
+
+  "me5": "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/131895930_1109695162842434_2796908455251723402_o.jpg?_nc_cat=103&ccb=2&_nc_sid=730e14&_nc_ohc=12Wt0kQVxxIAX_BdVeQ&_nc_ht=scontent-lga3-1.xx&oh=a57e8b5e020f2324984fb6c49b97ab67&oe=60008C2A",
+
+
+  /* Paragraphs */
+
+  "paragraph1": "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sedtellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+
+  "paragraph2": "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sedtellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+
+  "aboutMe": "Just me, myself and I, exploring the universe of uknownment. I have a heart of love and a interest of lorem ipsum and mauris neque quam blog. I want to share my world with you.",
+
+
+
+}
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
